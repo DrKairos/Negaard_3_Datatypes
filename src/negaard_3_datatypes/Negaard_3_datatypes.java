e1b8b1e6ebdeabc833c3f049e3e1fdb6b01e6cce
Eli Negaard <enegaard9169@stu.neisd.net>
2015-09-30 06:06:58 -0500

    Datatypes

---
+++ src/negaard_3_datatypes/Negaard_3_datatypes.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package negaard_3_datatypes;
+
+/**
+ *
+ * @author DrKairos
+ */
+public class Negaard_3_datatypes {
+
+    public static void main(String[] args) {
+        //This is a comment
+        //We will use many comments to explain our code
+        //This program explores some of the primitive data types
+        int i = 1;
+        float a = 2.5f;
+        double b = 3.87;
+        short c = 16;
+        char d1 = ';';
+        char d2 = 'a';
+        char d3 = 42;
+        char d4 = 145;
+        String e = d4+"";
+        String e1 = "Eli is the best";
+        boolean f = true;
+        boolean g = i % 2 == 0;
+        
+        
+        /*
+        here we will look at some mathematical operators
+        */
+        // + this adds two numbers and more
+        // - this subtracts two numbers and more
+        // * this multiplies two numbers and more
+        // / this divides two numbers and more
+        // % this is modulo. Shows the remainder
+        
+        
+//        System.out.println(i + " an integer");
+//        System.out.println(a + " a float");
+//        System.out.println(b + " a double");
+//        System.out.println(c + " a short");
+//        System.out.println(d1 + " a char");
+//        System.out.println(d2 + " a char");
+//        System.out.println(d3 + " a char");
+//        System.out.println(d4 + " a char");
+//        System.out.println(e + " a String");
+//        System.out.println(f + " a boolean");
+//        System.out.println(5 + " a boolean");
+//        for(i = -123;i < 123; i++) {
+//            //System.out.println((char) i);
+//             System.out.println(i % 3 == 0);
+//        }
+    // a number value for char
+    // maps to a character
+   i = 7;
+   a = 0;
+   e = "ninja";
+   
+   System.out.println(i + a + " an integer /" + "plus a float ");
+    System.out.println(a + b + " a float / " + "plus a double");
+    System.out.println(b + c + "a double /" + "plus a short ");
+    System.out.println(c + "a short");
+        
+    }
+    
+}
+
+
+// ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°)
+// ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°)
+// ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°)
+// ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°)
+// ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°)
+// ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°)
